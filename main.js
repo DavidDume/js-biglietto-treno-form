fa6d31811c2c8e70b0a5c9009dfd3b3282f034c2
David Dume <dumedavid11@gmail.com>
2023-02-23 16:20:04 +0100

    aggiunta biglietto html

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
 const generaBtn = document.querySelector('#generaBtn');
 const annullaBtn = document.querySelector('#annullaBtn');
+const biglietto = document.querySelector('#biglietto');
 
 generaBtn.addEventListener('click', function() {
- 
-    const kmInput = document.querySelector('#kilometri');
-    const etaInput = document.querySelector('#eta');
+
+    const nome = document.querySelector('#nome');
+    const tipo = document.querySelector('#tipo');
+    const carrozza = document.querySelector('#carrozza');
+    const codice = document.querySelector('#codice');
+    const prezzo = document.querySelector('#prezzo');
+    
+    const nomeInput = document.querySelector('#nomeForm');
+    const kmInput = document.querySelector('#kilometriForm');
+    const etaInput = document.querySelector('#etaForm');
     const km = parseInt(kmInput.value);
     const eta = parseInt(etaInput.value);
 
@@ -13,22 +21,35 @@ generaBtn.addEventListener('click', function() {
     } else {
         let costo = 0.21 * km;
         if(eta < 18) {
-            costo = (costo * 20) / 100;
+            tipo.innerHTML = "Under 18 <br/> Sconto del 20%"
+            costo = costo - ((costo * 20) / 100);
         } else if(eta >= 65) {
-            costo = (costo * 40) / 100;
+            tipo.innerHTML = "Over 65 <br/> Sconto del 40%"
+            costo = costo - ((costo * 40) / 100);
+        } else {
+            tipo.innerHTML = "Biglietto standard"
         }
-    
-        console.log(`Il costo totale è di ${costo.toFixed(2)} €`);
+        
+        nome.innerHTML = nomeInput.value;
+        carrozza.innerHTML = Math.floor(Math.random() * (1 - 6) + 6);
+        codice.innerHTML = Math.floor(Math.random() * (90000 - 99999) + 99999);
+        prezzo.innerHTML = `${costo.toFixed(2)} €`;
+
+        biglietto.classList.remove("hidden");
+        biglietto.classList.add("d-block");
+
     }
 })
 
 annullaBtn.addEventListener('click', function() {
-    const kmInput = document.querySelector('#kilometri');
-    const etaInput = document.querySelector('#eta');
-    const nomeInput = document.querySelector('#nome');
+    const kmInput = document.querySelector('#kilometriForm');
+    const etaInput = document.querySelector('#etaForm');
+    const nomeInput = document.querySelector('#nomeForm');
     kmInput.value = "";
     etaInput.value = "";
     nomeInput.value = "";
+    biglietto.classList.add("hidden");
+    biglietto.classList.remove("d-block");
 })
 
 
