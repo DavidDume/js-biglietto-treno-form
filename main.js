b5c28578c72e1b985468d99b3f1ef1764eff3926
David Dume <dumedavid11@gmail.com>
2023-02-23 15:39:15 +0100

    aggiunta tasto annulla e input per il nome

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
-const title = document.getElementById('title');
 const generaBtn = document.querySelector('#generaBtn');
-
+const annullaBtn = document.querySelector('#annullaBtn');
 
 generaBtn.addEventListener('click', function() {
  
@@ -8,16 +7,28 @@ generaBtn.addEventListener('click', function() {
     const etaInput = document.querySelector('#eta');
     const km = parseInt(kmInput.value);
     const eta = parseInt(etaInput.value);
-    let costo = 0.21 * km;
-    if(eta < 18) {
-        title.innerHTML = "Hai diritto allo sconto per minorenni!";
-        costo = (costo * 20) / 100;
-    } else if(eta >= 65) {
-        title.innerHTML = "Hai diritto allo sconto per anziani!";
-        costo = (costo * 40) / 100;
+
+    if(isNaN(km)) {
+        console.log('Inserisci numero valido!');
+    } else {
+        let costo = 0.21 * km;
+        if(eta < 18) {
+            costo = (costo * 20) / 100;
+        } else if(eta >= 65) {
+            costo = (costo * 40) / 100;
+        }
+    
+        console.log(`Il costo totale è di ${costo.toFixed(2)} €`);
     }
-    console.log(`Il costo totale è di ${costo.toFixed(2)} €`);
 })
 
+annullaBtn.addEventListener('click', function() {
+    const kmInput = document.querySelector('#kilometri');
+    const etaInput = document.querySelector('#eta');
+    const nomeInput = document.querySelector('#nome');
+    kmInput.value = "";
+    etaInput.value = "";
+    nomeInput.value = "";
+})
 
 
